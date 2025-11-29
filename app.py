c280ff8f31b50d519af8b68268fd70a08375d905
mohamadfarook <mohamadfarook.tops@gmail.com>
2025-11-29 09:44:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Student — farrok tops technologies Your Flask App is Running!"
+    return "Hello Student — farrok tops technologies Harsh Flask App is Running!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
